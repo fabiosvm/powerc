8d399ba972c7b40b1ee1253bba8ddcb0ccdabed8
Fábio de Souza Villaça Medeiros <fabiosvm@outlook.com>
2023-12-18 18:22:05 -0300

    Add Token

--- scanner/scanner.go
+++ scanner/scanner.go
@@ -1,1 +1,12 @@
 package scanner
+
+type Scanner interface {
+	// Returns the current token.
+	// TOKEN_KIND_EOF is returned if there are no more tokens.
+	CurrentToken() Token
+
+	// Go to the next token.
+	NextToken()
+}
+
+// TODO: Implement this interface.
